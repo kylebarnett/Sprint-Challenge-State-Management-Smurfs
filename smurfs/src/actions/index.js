8442128e45c4d9c1c4b2da136ad50285ed5f8375
Kyle Barnett <kbarnett23@gmail.com>
2019-09-28 11:35:06 -0600

    get smurfs seems to be working

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const FETCHING_SMURFS_START = "FETCHING_SMURFS_START";
+export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE"
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS_START })
+  axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data })
+    })
+}
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+import Smurf from './Smurf';
+
+const Smurfs = ({ getSmurfs, isFetching, smurfs }) => {
+  useEffect(() => {
+    getSmurfs()
+  }, [getSmurfs])
+  return (
+    <div>
+      {smurfs.map(smurf => (
+        <Smurf smurf={smurf} key={smurf.id} />
+      ))}
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    isFetching: state.isFetching
+  }
+}
+
+export default connect(mapStateToProps, { getSmurfs })(Smurfs)
